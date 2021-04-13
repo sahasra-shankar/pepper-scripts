541c903e4ac0c06d4a6d35a5c0f59d133dea55c7
SAHASRA SHANKAR <sahasra.shankar@gmail.com>
2021-04-12 19:09:22 -0700

    Working on argparse

--- command.py
+++ command.py
@@ -5,15 +5,15 @@ import argparse
 parser=argparse.ArgumentParser()
 
 # User specified input directory
-parser.add_argument('')
+parser.add_argument('-I', '--input_dir')
 # User specified output directory
-
+parser.add_argument('-O', '--output_dir')
 # User specified BAM
-
+parser.add_argument('-B', '--bam_file')
 # User specified REF
-
+parser.add_argument('-R', '--ref_file')
 # User specified output prefix
+parser.add_argument('-P', '--output_prefix')
 
-command="pwd"
-
-subprocess.check_call(command, shell=True, executable='/bin/bash')
+# command="pwd"
+# subprocess.check_call(command, shell=True, executable='/bin/bash')
