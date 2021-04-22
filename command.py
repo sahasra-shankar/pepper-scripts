643aae1b3082f05cb4114e1728f1013be7814747
SAHASRA SHANKAR <sahasra.shankar@gmail.com>
2021-04-22 12:42:01 -0700

    Implemented functions with params

--- command.py
+++ command.py
@@ -32,71 +32,77 @@ o_pre=args.output_prefix
 o_vcf=args.output_vcf
 threads=args.thread_num
 
-# define command
-command=[]
-command.append("docker")
-command.append("run")
-command.append("--ipc=host") # check if separate line --ipc=host
-command.append("-v")
-command.append(i_dir) # verify: 1 val or 2(input:/input)-> add line append (":")
-command.append("-v")
-command.append(o_dir) # verify: 1 val or 2(input:/input)
-command.append("kishwars/pepper_deepvariant:r0.4")
-command.append("run_pepper_margin_deepvariant call_variant")
-command.append("-b")
-command.append(i_dir)
-command.append("/")
-command.append(bam) # check if add line append ("/") needed
-command.append("-f")
-command.append(i_dir)
-command.append("/")
-command.append(bam)
-command.append("-t")
-command.append(threads)
-command.append("--ont")
+input_dir_volmount=i_dir. ':' .i_dir
+output_dir_volmount=o_dir. ':' .o_dir
+bam_path=i_dir + "/" + bam
+ref_path=i_dir + "/" + ref
 
-# subprocess.check_call(command, shell=True, executable='/bin/bash')
+# define command
+def default_command(input_dir_volmount, output_dir_volmount, bam_path, ref_path, threads)
+    command=[]
+    command.append("docker")
+    command.append("run")
+    command.append("--ipc=host") # check if separate line --ipc=host
+    command.append("-v")
+    command.append(input_dir_volmount) 
+    command.append("-v")
+    command.append(output_dir_volmount) 
+    command.append("kishwars/pepper_deepvariant:r0.4")
+    command.append("run_pepper_margin_deepvariant call_variant")
+    command.append("-b")
+    command.append(bam_path)
+    command.append("-f")
+    command.append(ref_path)
+    command.append("-t")
+    command.append(threads)
+    command.append("--ont")
+    return command
 
 #define truth vcf and bed
 truth_vcf="HG002_GRCh38_1_22_v4.2.1_benchmark.vcf.gz"
 truth_bed="HG002_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed"
 
-#define optional command hap.py
-opt_command=[]
-opt_command.append("docker")
-opt_command.append("run")
-opt_command.append("-it") # check if separate line --ipc=host
-opt_command.append("-v")
-opt_command.append(i_dir) # verify: 1 val or 2(input:/input)-> add line append (":")
-opt_command.append("-v")
-opt_command.append(o_dir) # verify: 1 val or 2(input:/input)
-opt_command.append("jmcdani20/hap.py:v0.3.12")
-opt_command.append("/opt/hap.py/bin/hap.py")
-opt_command.append(i_dir)
-opt_command.append(truth_vcf)
-opt_command.append(o_dir)
-opt_command.append(o_vcf)
+#path to truth vcf
+truth_vcf_path= i_dir + "/" + truth_vcf
+#path to output vcf
+output_vcf_path= o_dir + "/" + o_vcf
+#path to truth bed
+truth_bed_path= i_dir + "/" + truth_bed
 
-opt_command.append("-f")
-opt_command.append(i_dir)
-opt_command.append("/")
-opt_command.append(truth_bed)
-opt_command.append("-r")
-opt_command.append(i_dir)
-opt_command.append("/")
-opt_command.append(ref)
-opt_command.append("-o")
-opt_command.append(o_dir)
-opt_command.append("/happy.output")
-opt_command.append("--pass-only")
-opt_command.append("-l")
-opt_command.append("chr20") # quotes or no quotes
-opt_command.append("--engine")
-# opt_command.append("=")
-opt_command.append("vcfeval") # quotes or no quotes
-opt_command.append("--threads")
-# opt_command.append("=")
-opt_command.append(threads)
+#define optional command hap.py
+def optional_command(input_dir_mount, output_dir_mount, truth_vcf_path, output_vcf_path, truth_bed_path, ref_path, o_dir, threads)
+    opt_command=[]
+    opt_command.append("docker")
+    opt_command.append("run")
+    opt_command.append("-it") # check if separate line --ipc=host
+    opt_command.append("-v")
+    opt_command.append(input_dir_mount) 
+    opt_command.append("-v")
+    opt_command.append(output_dir_mount) 
+    opt_command.append("jmcdani20/hap.py:v0.3.12")
+    opt_command.append("/opt/hap.py/bin/hap.py")
+    opt_command.append(truth_vcf_path)
+    opt_command.append(output_vcf_path)
+    opt_command.append("-f")
+    opt_command.append(truth_bed_path)
+    opt_command.append("-r")
+    opt_command.append(ref_path)
+    opt_command.append("-o")
+    opt_command.append(o_dir)
+    opt_command.append("/happy.output")
+    opt_command.append("--pass-only")
+    opt_command.append("-l")
+    opt_command.append("chr20") # quotes or no quotes
+    opt_command.append("--engine")
+    # opt_command.append("=")
+    opt_command.append("vcfeval") # quotes or no quotes
+    opt_command.append("--threads")
+    # opt_command.append("=")
+    opt_command.append(threads)
+    return opt_command
 
 if args.with_margin:
+    # subprocess.check_call(default_command, shell=True, executable='/bin/bash')
     # subprocess.check_call(opt_command, shell=True, executable='/bin/bash')
+else:
+    # subprocess.check_call(default_command, shell=True, executable='/bin/bash')
